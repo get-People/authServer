9aba1cb14c58e36c94a5ce29e968e2b77c61d5bd
israelmark <israelmark98@gmail.com>
2024-03-20 15:25:10 +0200

    feat(): creating a register and login requsts

---
+++ authServer.ts
@@ -0,0 +1,45 @@
+import express from 'express';
+import http from 'http';
+import purify from "./utils/sanitize";
+import { authValidator } from './validation/authValidator';
+import User from './models/user';
+const app = express();
+
+app.use(express.json())
+
+const server = http.createServer(app)
+
+app.post("/register", async (req, res) => {
+    try {
+        Object.keys(req.body).forEach(key => {
+            req.body[key] = purify.sanitize(req.body[key])
+        })
+        const { error } = authValidator.validate(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+
+        const newUser = await User.create(req.body);
+        res.status(200).send(newUser);
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({ errorMessage: "register fail" })
+    }
+})
+
+app.post("/login", (req,res) => {
+    try {
+        Object.keys(req.body).forEach(key => {
+            req.body[key] = purify.sanitize(req.body[key])
+        })
+
+        const user = User.findOne(req.body["email"]) 
+        if (!user) return res.status(400).send({ message: "User not found" })
+        return res.status(200).send({ message: "access authorized"})
+    } catch (error) {
+          console.log(error)
+        res.status(500).send({ errorMessage: "login fail" })
+    }
+})
+
+server.listen(process.env.PORT as string,() => {
+     console.log(`server is listening on port ${process.env.PORT}`)
+ })
---
+++ validation/authValidator.ts
@@ -0,0 +1,14 @@
+import Joi from 'joi';
+
+export const authValidator = Joi.object({
+    firstName: Joi.string().min(3).max(30).required(),
+    lastName: Joi.string().min(3).max(30).required(),
+    email: Joi.string().min(15).max(30).email().required(),
+    address: Joi.object({
+        country_id: Joi.string().required(),
+        city_id: Joi.string().required(),
+        street_id: Joi.string().required(),
+    }).required(),
+    age: Joi.number().min(18).max(120).required(),
+})
+
